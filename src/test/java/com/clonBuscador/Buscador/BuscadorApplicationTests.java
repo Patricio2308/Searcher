b132675ef0a826dc456c2179fc03092b9051b89f
Patricio2308 <patricioandres00@gmail.com>
2025-01-10 13:07:45 -0300

    first commit

---
+++ src/test/java/com/clonBuscador/Buscador/BuscadorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clonBuscador.Buscador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuscadorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
